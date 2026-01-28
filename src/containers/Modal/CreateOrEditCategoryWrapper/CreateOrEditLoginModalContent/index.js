f76376818a0492f9a64ef40c418d036c500ad354
Denis Lupookov <denislupookov@gmail.com>
2026-01-28 13:26:24 +0200

    refactor

--- src/containers/Modal/CreateOrEditCategoryWrapper/CreateOrEditLoginModalContent/index.js
+++ src/containers/Modal/CreateOrEditCategoryWrapper/CreateOrEditLoginModalContent/index.js
@@ -175,7 +175,7 @@ export const CreateOrEditLoginModalContent = ({
       folder: values.folder,
       isFavorite: initialRecord?.isFavorite ?? isFavorite,
       data: {
-        ...initialRecord?.data,
+        ...(initialRecord?.data ? initialRecord.data : {}),
         title: values.title,
         username: values.username,
         password: values.password,
