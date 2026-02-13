e2e52bd3fc2453318f36b3815348687b60f4a1e2
Tornike Kapanadze <tornikekapan@gmail.com>
2026-02-13 12:03:21 +0200

    restore dev code

--- src/pages/SettingsView/SettingsVaultsTab/index.js
+++ src/pages/SettingsView/SettingsVaultsTab/index.js
@@ -1,38 +1,39 @@
 import { useLingui } from '@lingui/react'
 import { html } from 'htm/react'
-import { useVault } from 'pearpass-lib-vault'
+import { useVaults } from 'pearpass-lib-vault'
 
 import { Content } from './styles'
 import { CardSingleSetting } from '../../../components/CardSingleSetting'
 import { ListItem } from '../../../components/ListItem'
 import { ModifyVaultModalContent } from '../../../containers/Modal/ModifyVaultModalContent'
 import { useModal } from '../../../context/ModalContext'
+import { sortByName } from '../../../utils/sortByName'
 import { vaultCreatedFormat } from '../../../utils/vaultCreated'
 
 export const SettingsVaultsTab = () => {
   const { i18n } = useLingui()
-  const { data: vault } = useVault()
+  const { data } = useVaults()
   const { setModal } = useModal()
 
   return html`
-    <${CardSingleSetting}
-      title=${i18n._('Your Vault')}
-      description=${i18n._('Share, edit, or delete your vault from one place.')}
-    >
+    <${CardSingleSetting} title=${i18n._('Manage Vaults')}>
       <${Content}>
-        <${ListItem}
-          key=${vault.name}
-          itemName=${vault.name}
-          itemDateText=${vaultCreatedFormat(vault.createdAt)}
-          onEditClick=${() =>
-            setModal(
-              html`<${ModifyVaultModalContent}
-                vaultId=${vault.id}
-                vaultName=${vault.name}
-              />`
-            )}
-        />
+        ${sortByName(data).map(
+          (vault) =>
+            html`<${ListItem}
+              key=${vault.name}
+              itemName="${vault.name}"
+              itemDateText=${vaultCreatedFormat(vault.createdAt)}
+              onEditClick=${() =>
+                setModal(
+                  html`<${ModifyVaultModalContent}
+                    vaultId=${vault.id}
+                    vaultName=${vault.name}
+                  />`
+                )}
+            />`
+        )}
       <//>
-    </${CardSingleSetting}>
+    <//>
   `
 }
