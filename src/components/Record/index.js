e47e1cbbb2d6b23f9795f63772608352155bcdf5
Stefan Nikolic <68122840+stefannikolic@users.noreply.github.com>
2026-02-27 17:22:07 +0100

    eslint fix

--- src/components/Record/index.js
+++ src/components/Record/index.js
@@ -102,7 +102,10 @@ export const Record = ({
             <${RecordActionsPopupContent} menuItems=${actions} />
           `}
         >
-          <div onClick=${handleActionMenuToggle} data-testid="list-item-threedots">
+          <div
+            onClick=${handleActionMenuToggle}
+            data-testid="list-item-threedots"
+          >
             <${KebabMenuIcon} />
           </div>
         <//>
--- src/lib-react-components/components/PearPassPasswordField/index.js
+++ src/lib-react-components/components/PearPassPasswordField/index.js
@@ -2,10 +2,6 @@ import { useState } from 'react'
 
 import { html } from 'htm/react'
 
-import { EyeClosedIcon } from '../../icons/EyeClosedIcon'
-import { EyeIcon } from '../../icons/EyeIcon'
-import { LockCircleIcon } from '../../icons/LockCircleIcon'
-import { ButtonRoundIcon } from '../ButtonRoundIcon'
 import {
   AdditionalItems,
   IconWrapper,
@@ -15,6 +11,10 @@ import {
   MainWrapper,
   NoticeWrapper
 } from './styles'
+import { EyeClosedIcon } from '../../icons/EyeClosedIcon'
+import { EyeIcon } from '../../icons/EyeIcon'
+import { LockCircleIcon } from '../../icons/LockCircleIcon'
+import { ButtonRoundIcon } from '../ButtonRoundIcon'
 import { NoticeText } from '../NoticeText'
 
 /**
