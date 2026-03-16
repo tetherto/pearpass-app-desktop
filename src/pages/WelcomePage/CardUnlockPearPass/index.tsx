e02f8e1182ac2da95d256098fe9d53f005f125fd
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-03-16 17:47:21 +0400

    Add V2 auto-vault creation flow in CardUnlockPearPass.

--- src/pages/WelcomePage/CardUnlockPearPass/index.tsx
+++ src/pages/WelcomePage/CardUnlockPearPass/index.tsx
@@ -1,19 +1,46 @@
-import { useUserData, useVaults } from 'pearpass-lib-vault'
+// @ts-ignore - V2_DESIGN_ENABLED is added locally to pearpass-lib-constants
+import { V2_DESIGN_ENABLED } from 'pearpass-lib-constants'
+// @ts-ignore - some hooks not resolved by TS due to re-export chain
+import { useCreateVault, useUserData, useVault, useVaults } from 'pearpass-lib-vault'
 
 import { AlertBox } from '../../../components/AlertBox'
 import { NAVIGATION_ROUTES } from '../../../constants/navigation'
 import { AuthenticationCard } from '../../../containers/AuthenticationCard'
 import { useRouter } from '../../../context/RouterContext'
 import { useTranslation } from '../../../hooks/useTranslation'
+import { getDeviceName } from '../../../utils/getDeviceName'
+import { logger } from '../../../utils/logger'
 
 export const CardUnlockPearPass = () => {
   const { t } = useTranslation()
   const { currentPage, navigate } = useRouter()
-  const { initVaults } = useVaults()
+  const { initVaults, refetch: refetchVaults } = useVaults()
   const { refreshMasterPasswordStatus } = useUserData()
+  const { createVault } = useCreateVault()
+  const { addDevice, refetch: refetchVault } = useVault()
 
   const handleSuccess = async (password: string) => {
     await initVaults({ password })
+
+    if (V2_DESIGN_ENABLED) {
+      try {
+        const freshVaults = await refetchVaults()
+
+        if (!freshVaults || freshVaults.length === 0) {
+          await createVault({ name: t('Personal') })
+          await addDevice(getDeviceName())
+          navigate('vault', { recordType: 'all' })
+        } else {
+          await refetchVault(freshVaults[0].id)
+          navigate('vault', { recordType: 'all' })
+        }
+      } catch (error) {
+        logger.error('CardUnlockPearPass', 'Error during V2 auto-vault:', error)
+        navigate(currentPage, { state: 'vaults' })
+      }
+      return
+    }
+
     navigate(currentPage, { state: 'vaults' })
   }
 
