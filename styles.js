962f585a7fcdb8702d6dbe27576a8c786a10f308
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-03-16 17:43:29 +0400

    Wrap global CSS reset in @layer for react-strict-dom compatibility.

--- styles.js
+++ styles.js
@@ -21,170 +21,172 @@ export const setFontsAndResetCSS = () => {
         format('truetype');
     }
 
-    pear-ctrl[data-platform='darwin'] {
-      margin-top: 12px;
-      margin-left: 10px;
-    }
+    @layer reset {
+      pear-ctrl[data-platform='darwin'] {
+        margin-top: 12px;
+        margin-left: 10px;
+      }
 
-    html,
-    body,
-    div,
-    span,
-    applet,
-    object,
-    iframe,
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6,
-    p,
-    blockquote,
-    pre,
-    a,
-    abbr,
-    acronym,
-    address,
-    big,
-    cite,
-    code,
-    del,
-    dfn,
-    em,
-    img,
-    ins,
-    kbd,
-    q,
-    s,
-    samp,
-    small,
-    strike,
-    strong,
-    sub,
-    sup,
-    tt,
-    var,
-    b,
-    u,
-    i,
-    center,
-    dl,
-    dt,
-    dd,
-    ol,
-    ul,
-    li,
-    fieldset,
-    form,
-    label,
-    legend,
-    table,
-    caption,
-    tbody,
-    tfoot,
-    thead,
-    tr,
-    th,
-    td,
-    article,
-    aside,
-    canvas,
-    details,
-    embed,
-    figure,
-    figcaption,
-    footer,
-    header,
-    hgroup,
-    menu,
-    nav,
-    output,
-    ruby,
-    section,
-    summary,
-    time,
-    mark,
-    audio,
-    video {
-      margin: 0;
-      padding: 0;
-      border: 0;
-      font-size: 100%;
-      font: inherit;
-      vertical-align: baseline;
-    }
-    article,
-    aside,
-    details,
-    figcaption,
-    figure,
-    footer,
-    header,
-    hgroup,
-    menu,
-    nav,
-    section {
-      display: block;
-    }
+      html,
+      body,
+      div,
+      span,
+      applet,
+      object,
+      iframe,
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6,
+      p,
+      blockquote,
+      pre,
+      a,
+      abbr,
+      acronym,
+      address,
+      big,
+      cite,
+      code,
+      del,
+      dfn,
+      em,
+      img,
+      ins,
+      kbd,
+      q,
+      s,
+      samp,
+      small,
+      strike,
+      strong,
+      sub,
+      sup,
+      tt,
+      var,
+      b,
+      u,
+      i,
+      center,
+      dl,
+      dt,
+      dd,
+      ol,
+      ul,
+      li,
+      fieldset,
+      form,
+      label,
+      legend,
+      table,
+      caption,
+      tbody,
+      tfoot,
+      thead,
+      tr,
+      th,
+      td,
+      article,
+      aside,
+      canvas,
+      details,
+      embed,
+      figure,
+      figcaption,
+      footer,
+      header,
+      hgroup,
+      menu,
+      nav,
+      output,
+      ruby,
+      section,
+      summary,
+      time,
+      mark,
+      audio,
+      video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+      }
+      article,
+      aside,
+      details,
+      figcaption,
+      figure,
+      footer,
+      header,
+      hgroup,
+      menu,
+      nav,
+      section {
+        display: block;
+      }
 
-    * {
-      box-sizing: border-box;
-    }
-    html {
-      height: 100%;
-      display: flex;
-      padding-top: var(--title-bar-height);
-    }
-    body {
-      line-height: 1;
-      flex: 1;
-    }
-    #root {
-      height: 100%;
-    }
-    #bar {
-      backdrop-filter: blur(64px);
-      -webkit-app-region: drag;
-      height: var(--title-bar-height);
-      padding: 0;
-      color: ${({ theme }) => theme.colors.white.mode1};
-      white-space: nowrap;
-      position: fixed;
-      z-index: 10000;
-      width: 100%;
-      left: 0;
-      top: 0;
-    }
-    ol,
-    ul {
-      list-style: none;
-    }
-    blockquote,
-    q {
-      quotes: none;
-    }
-    blockquote:before,
-    blockquote:after,
-    q:before,
-    q:after {
-      content: '';
-      content: none;
-    }
-    table {
-      border-collapse: collapse;
-      border-spacing: 0;
-    }
-    input {
-      background-color: transparent;
-      border: none;
-    }
-    input:focus {
-      outline: none;
-    }
+      * {
+        box-sizing: border-box;
+      }
+      html {
+        height: 100%;
+        display: flex;
+        padding-top: var(--title-bar-height);
+      }
+      body {
+        line-height: 1;
+        flex: 1;
+      }
+      #root {
+        height: 100%;
+      }
+      #bar {
+        backdrop-filter: blur(64px);
+        -webkit-app-region: drag;
+        height: var(--title-bar-height);
+        padding: 0;
+        color: ${({ theme }) => theme.colors.white.mode1};
+        white-space: nowrap;
+        position: fixed;
+        z-index: 10000;
+        width: 100%;
+        left: 0;
+        top: 0;
+      }
+      ol,
+      ul {
+        list-style: none;
+      }
+      blockquote,
+      q {
+        quotes: none;
+      }
+      blockquote:before,
+      blockquote:after,
+      q:before,
+      q:after {
+        content: '';
+        content: none;
+      }
+      table {
+        border-collapse: collapse;
+        border-spacing: 0;
+      }
+      input {
+        background-color: transparent;
+        border: none;
+      }
+      input:focus {
+        outline: none;
+      }
 
-    *::-webkit-scrollbar {
-      display: none;
+      *::-webkit-scrollbar {
+        display: none;
+      }
     }
   `)
 }
