02fc1f42fe38c4d536077d321d388325653973bf
Alvaro Paco <alvaro.paco@tether.io>
2026-03-19 09:05:01 -0300

    reverting windows related changes

--- electron/main.cjs
+++ electron/main.cjs
@@ -70,7 +70,7 @@ let vaultClient = null
 function getExecPath() {
   if (!app.isPackaged) return null
   if (isLinux && process.env.APPIMAGE) return process.env.APPIMAGE
-  if (isWindows) return process.execPath
+  if (isWindows) true
   return path.join(process.resourcesPath, '..', '..')
 }
 
